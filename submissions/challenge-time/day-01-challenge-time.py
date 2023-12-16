15f69af72a388a8c25adbf17b70161a8a571d519
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2023-12-16 17:05:03 +0800

    DAY 1: Done with challenge time

---
+++ submissions/challenge-time/day-01-challenge-time.py
@@ -0,0 +1,16 @@
+my_name = {
+    "first_name": "David",
+    "last_name": "Bato bato"
+}
+
+name_of_department = "Department of Software and Web Development"
+about_me = "I love to code and learn new things."
+
+intro_message = (
+    f"""
+Hello, I am {my_name['first_name']} {my_name['last_name']}. I love {name_of_department} and I love being in the Backend even more!
+One thing about me is that {about_me}.
+    """
+)
+
+print(intro_message)
